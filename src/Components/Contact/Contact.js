caaf01760a5074073332a60c211fe7398342449a
[01;31m[K"name"[m[K: "Marissa Tomko <[01;31m[K"email"[m[K: null>
2018-11-01 17:47:44 -0700

    Add contact form - still needs styling

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import './Contact.css';
 
 class Contact extends React.Component {
   render() {
     return(
-      <div>
-        <h1>Contact</h1>
+      <div className="contact-div">
+        <div className="contact-heading">
+          <h1>Contact</h1>
+          <hr/>
+        </div>
+
+
+        <p className="contact-text">
+          Have some input about our suggestions? Recommendations of your own?
+          Just think we look cool AF (we are)? Get in touch!
+        </p>
+
+        <div className="contact-form">
+
+          <form action="https://formspree.io/marissatomko@gmail.com" method="POST">
+            <input type="text" name="message" placeholder="What's up?" />
+            <input type="email" name="replyto" placeholder="What's your email?" />
+
+            <input type="hidden" name="_subject" value="Contact form" />
+
+            <input type="submit" value="Send" />
+          </form>
+
+        </div>
+
+
       </div>
     )
   }
