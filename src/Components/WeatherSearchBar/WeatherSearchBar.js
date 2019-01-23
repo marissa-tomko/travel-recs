799ae0801a77bec3cba7f0e8a0f657334a43d3ad
[01;31m[K"name"[m[K: "Marissa Tomko <[01;31m[K"email"[m[K: null>
2019-01-23 10:05:02 -0800

    Change fetch to https

--- src/Components/WeatherSearchBar/WeatherSearchBar.js
+++ src/Components/WeatherSearchBar/WeatherSearchBar.js
@@ -24,7 +24,7 @@ class WeatherSearchBar extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=imperial`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&units=imperial`);
     const data = await api_call.json();
     if (city && country) {
       this.setState({
